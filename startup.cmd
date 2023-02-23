30dc218864d56c7689faa17bad7750481c99a54c
Robin Universe <mail@robinuniver.se>
2023-02-23 07:46:04 -0600

    Create startup.cmd

---
+++ startup.cmd
@@ -0,0 +1,6 @@
+:: Powershell Starter
+:: Put this in %appdata%\Microsoft\Windows\Start Menu\Programs\Startup\
+:: Robin Universe [X]
+:: 02 . 23 . 23
+
+powershell -windowstyle hidden C:\Scenes\startup.ps1
