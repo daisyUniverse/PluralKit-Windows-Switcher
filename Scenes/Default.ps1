2fd39a3920e496277c96bf25f212239274c4ed37
Robin Universe <mail@robinuniver.se>
2023-02-23 08:00:40 -0600

    Create Default.ps1

---
+++ Scenes/Default.ps1
@@ -0,0 +1,6 @@
+# Default Per-Alter Startup Script
+# This script will fire off once whenever startup.ps1 finishes
+# To make one for each alter just name the file [altername].ps1 and place it in this folder
+# So you can do some one off stuff in here that you'd rather not do in the PS Profile
+# Robin Universe [X]
+# 02 . 23 . 23
